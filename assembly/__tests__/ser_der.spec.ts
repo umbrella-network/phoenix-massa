b32bac40c707c08dd31c4f535a51d2ee049b004f
sydhds <sydhds@gmail.com>
2024-02-29 11:02:52 +0100

    Improve priceData ser / der unit tests (round 2)

--- assembly/__tests__/ser_der.spec.ts
+++ assembly/__tests__/ser_der.spec.ts
@@ -3,7 +3,7 @@ import { u128 } from 'as-bignum/assembly';
 import {PriceData} from "../contracts/onChainFeeds/UmbrellaFeedsCommon";
 
 describe("priceData ser / der", () => {
-    it("", () => {
+    it("priceData regular serialize then deserialize", () => {
         let priceData = new PriceData(1, 25000, 61527, u128.from(1037));
 
         let ser = priceData.serialize();
@@ -19,4 +19,20 @@ describe("priceData ser / der", () => {
         expect(priceData).toStrictEqual(priceData3);
         expect(priceData2).toStrictEqual(priceData3);
     });
+
+    it("priceData try_deserialize invalid data", () => {
+
+        // Check PriceData.try_deserialize will not throw on error
+
+        let priceData = new PriceData(1, 25042, 60927, u128.from(1837));
+        let ser = priceData.serialize();
+        let priceData2: PriceData = new PriceData();
+        // Note: slice serialized data to simulate an invalid buffer
+        let invalid_ser = ser.slice<StaticArray<u8>>(1, 9);
+        let res = priceData2.try_deserialize(invalid_ser);
+
+        expect<bool>(res.isErr()).toBeTruthy();
+        expect(priceData2).toStrictEqual(new PriceData());
+    });
+
 });
