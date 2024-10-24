1e7f69863fbabc900c87d3901b341090bf213f96
DZariusz <dzariusz@gmail.com>
2024-10-24 15:55:38 +0200

    temporary bank

--- assembly/contracts/stakingBankStatic/StakingBankStaticProd.ts
+++ assembly/contracts/stakingBankStatic/StakingBankStaticProd.ts
@@ -19,6 +19,32 @@ import {
 const VALIDATOR_0: string = "P12W6zgQb5aykbYSz4CfQLuk3axRcp5jYX1fmBT7VRdgVnzv6oHH";
 // Validator 1 Public Key
 const VALIDATOR_1: string = "P1QV6AsrtPdZqk9BVyMMfFCDgYEaMaSBatnCk8xs1jJzcMGNeFa";
+// temp
+const VALIDATOR_2: string = "P1QV6AsrtPdZqk9BVyMMfFCDgYEaMaSBatnCk8xs1jJzcMGNeFa";
+// temp
+const VALIDATOR_3: string = "P1QV6AsrtPdZqk9BVyMMfFCDgYEaMaSBatnCk8xs1jJzcMGNeFa";
+// temp
+const VALIDATOR_4: string = "P1QV6AsrtPdZqk9BVyMMfFCDgYEaMaSBatnCk8xs1jJzcMGNeFa";
+// temp
+const VALIDATOR_5: string = "P1QV6AsrtPdZqk9BVyMMfFCDgYEaMaSBatnCk8xs1jJzcMGNeFa";
+// temp
+const VALIDATOR_6: string = "P1QV6AsrtPdZqk9BVyMMfFCDgYEaMaSBatnCk8xs1jJzcMGNeFa";
+// temp
+const VALIDATOR_7: string = "P1QV6AsrtPdZqk9BVyMMfFCDgYEaMaSBatnCk8xs1jJzcMGNeFa";
+// temp
+const VALIDATOR_8: string = "P1QV6AsrtPdZqk9BVyMMfFCDgYEaMaSBatnCk8xs1jJzcMGNeFa";
+// temp
+const VALIDATOR_9: string = "P1QV6AsrtPdZqk9BVyMMfFCDgYEaMaSBatnCk8xs1jJzcMGNeFa";
+// temp
+const VALIDATOR_10: string = "P1QV6AsrtPdZqk9BVyMMfFCDgYEaMaSBatnCk8xs1jJzcMGNeFa";
+// temp
+const VALIDATOR_11: string = "P1QV6AsrtPdZqk9BVyMMfFCDgYEaMaSBatnCk8xs1jJzcMGNeFa";
+// temp
+const VALIDATOR_12: string = "P1QV6AsrtPdZqk9BVyMMfFCDgYEaMaSBatnCk8xs1jJzcMGNeFa";
+// temp
+const VALIDATOR_13: string = "P1QV6AsrtPdZqk9BVyMMfFCDgYEaMaSBatnCk8xs1jJzcMGNeFa";
+// temp
+const VALIDATOR_14: string = "P1QV6AsrtPdZqk9BVyMMfFCDgYEaMaSBatnCk8xs1jJzcMGNeFa";
 
 
 class StakingBankStaticProd extends StakingBankStatic {
@@ -32,7 +58,46 @@ class StakingBankStaticProd extends StakingBankStatic {
             return new Validator(_id, "https://validator.umb.network");
         }
         if (_id == VALIDATOR_1) {
-            return new Validator(_id, "https://validator2.umb.network/");
+            return new Validator(_id, "https://validator2.umb.network");
+        }
+        if (_id == VALIDATOR_2) {
+            return new Validator(_id, "https://umbrella.artemahr.tech");
+        }
+        if (_id == VALIDATOR_3) {
+            return new Validator(_id, "https://umb.vtabsolutions.com:3030");
+        }
+        if (_id == VALIDATOR_4) {
+            return new Validator(_id, "https://umbrella.crazywhale.es");
+        }
+        if (_id == VALIDATOR_5) {
+            return new Validator(_id, "https://umbrella-node.gateomega.com");
+        }
+        if (_id == VALIDATOR_6) {
+            return new Validator(_id, "https://umb.anorak.technology");
+        }
+        if (_id == VALIDATOR_7) {
+            return new Validator(_id, "https://umbrella.validator.infstones.io");
+        }
+        if (_id == VALIDATOR_8) {
+            return new Validator(_id, "https://umb.hashkey.cloud");
+        }
+        if (_id == VALIDATOR_9) {
+            return new Validator(_id, "http://umbrella.staking4all.org:3000");
+        }
+        if (_id == VALIDATOR_10) {
+            return new Validator(_id, "http://5.161.78.230:3000");
+        }
+        if (_id == VALIDATOR_11) {
+            return new Validator(_id, "https://umb-api.staking.rocks");
+        }
+        if (_id == VALIDATOR_12) {
+            return new Validator(_id, "https://rpc.urbanhq.net");
+        }
+        if (_id == VALIDATOR_13) {
+            return new Validator(_id, "https://umbrella-node.ankastake.com");
+        }
+        if (_id == VALIDATOR_14) {
+            return new Validator(_id, "https://umbrella.tchambrella.com");
         }
 
         return new Validator("", "");
@@ -44,14 +109,39 @@ class StakingBankStaticProd extends StakingBankStatic {
 
         list[0] = VALIDATOR_0;
         list[1] = VALIDATOR_1;
+        list[2] = VALIDATOR_2;
+        list[3] = VALIDATOR_3;
+        list[4] = VALIDATOR_4;
+        list[5] = VALIDATOR_5;
+        list[6] = VALIDATOR_6;
+        list[7] = VALIDATOR_7;
+        list[8] = VALIDATOR_8;
+        list[9] = VALIDATOR_9;
+        list[10] = VALIDATOR_10;
+        list[11] = VALIDATOR_11;
+        list[12] = VALIDATOR_12;
+        list[13] = VALIDATOR_13;
+        list[14] = VALIDATOR_14;
 
         return list;
     }
 
     _isValidator(_validator: string): bool {
-        // TODO/ all validator
         return (_validator == VALIDATOR_0
             || _validator == VALIDATOR_1
+            || _validator == VALIDATOR_2
+            || _validator == VALIDATOR_3
+            || _validator == VALIDATOR_4
+            || _validator == VALIDATOR_5
+            || _validator == VALIDATOR_6
+            || _validator == VALIDATOR_7
+            || _validator == VALIDATOR_8
+            || _validator == VALIDATOR_9
+            || _validator == VALIDATOR_10
+            || _validator == VALIDATOR_11
+            || _validator == VALIDATOR_12
+            || _validator == VALIDATOR_13
+            || _validator == VALIDATOR_14
         );
     }
 }
